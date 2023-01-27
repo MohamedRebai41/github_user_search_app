c6f0bcad6cb54f8b70ffc90a66e58cb3551eb0e7
Rebai Mohamed <fankoush@Fankoush.localdomain>
2023-01-27 12:25:03 +0100

    Multiple changes: 	added search bar design and logic 	added communication with github api 	started working on rendering the data received

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-let themebtn = document.getElementById("chtheme");
-themebtn.addEventListener("click", function() {
-    
+let ic=document.getElementById("ic");
+let bar=document.getElementById("searchbar");
+let disp=document.getElementById("display");
+let btn1=document.getElementById("btn1");
+ic.addEventListener("click",
+()=>
+{
+    bar.focus();
+});
+
+btn1.addEventListener("click",
+()=>
+{
+    disp.textContent="";
+    let name=bar.value;
+    bar.value="";
+    let newname=name.split(' ').join('');
+
+    fetch('https://api.github.com/users/'+ newname)
+    .then((result)=> result.json())
+    .then((data)=>{
+    let im=document.createElement("img");
+    im.src=data.avatar_url;
+    im.id="img"
+    im.alt="avatar"
+    disp.appendChild(im);
+    })
+
 })
